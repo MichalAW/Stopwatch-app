2ba478faaf952701cd12eb4397fc206bd4cb0186
MichalAW <michal.wanta@gmail.com>
2019-05-13 22:19:27 +0200

    add start and watch

--- script.js
+++ script.js
@@ -22,9 +22,35 @@ class Stopwatch {
         return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
     };
 
-    start() {}
+    start() {
+        if (!this.running) {
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
+        }
+    }
 
-    stop() {}
+    step() {
+        if (!this.running) return;
+        this.calculate();
+        this.print();
+    }
+
+    calculate() {
+        this.times.miliseconds += 1;
+        if (this.times.miliseconds >= 100) {
+            this.times.seconds += 1;
+            this.times.miliseconds = 0;
+        }
+        if (this.times.seconds >= 60) {
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
+    }
+
+    stop() {
+        this.running = false;
+        clearInterval(this.watch);
+    }
 }
 
 function pad0(value) {
@@ -44,4 +70,3 @@ stopButton.addEventListener('click', () => stopwatch.stop());
 
 const stopwatch = new Stopwatch(
     document.querySelector('.stopwatch'));
-
